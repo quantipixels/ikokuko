d4f3d4a07ae38f25820ee979b38dc01929979d3e
Emmanuel Sobande <eosobande@gmail.com>
2025-11-12 23:50:01 +0000

    change central publishing to OSSRH

--- ikokuko/build.gradle.kts
+++ ikokuko/build.gradle.kts
@@ -69,8 +69,8 @@ publishing {
 
     repositories {
         maven {
-            name = "CentralPortal"
-            url = uri("https://central.sonatype.com/api/v1/publisher/deploy")
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
                 username = System.getenv("MAVEN_CENTRAL_USERNAME")
                 password = System.getenv("MAVEN_CENTRAL_PASSWORD")
